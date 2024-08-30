5d1058c4bf12f5d3393556de822e3aeabe2ef73c
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2024-08-30 17:01:10 +0300

    add license

--- src/components/Licenses/Licenses.jsx
+++ src/components/Licenses/Licenses.jsx
@@ -2,44 +2,60 @@ import style from "./Licenses.module.scss";
 import Image from "next/image";
 
 export default function Licenses() {
-    return (
-        <section className="section">
-            <div className="container">
+  return (
+    <section className={`section ${style.section__license}`}>
+      <div className="container">
+        <div className={style.box}>
+          <div>
+            <h2 className="title">
+              Licenses: Rocket Play Casino No Deposit Bonus Codes 2022
+            </h2>
+            <p>
+              The service operates legally. The confirmation is a valid Curacao
+              license. Information about the license is available on the
+              official website.
+            </p>
+          </div>
+          <div>
+            <Image
+              src="/images/licence_logo.webp"
+              alt="license"
+              width={240}
+              height={240}
+              loading="lazy"
+              className={style.img}
+            />
+          </div>
 
-                <div className={style.box}>
-                    <div>
-                        <h2 className="title">Licenses</h2>
-                        <p>Grey Eagle Casino operates under the licensing and regulatory oversight of the Alberta
-                            Gaming,
-                            Liquor, and Cannabis Commission (AGLC). The licensing by the AGLC signifies that Grey Eagle
-                            Casino upholds high standards of integrity, security, and responsible gaming practices,
-                            providing guests with confidence in the legitimacy and reliability of the casino
-                            operations.</p>
-
-                    </div>
-                    <div><Image src="/images/aglc.jpeg" alt="license" width={280} height={186} loading="lazy"/></div>
-
-                    <div>
-                        <h2 className="title">Software Technologies</h2>
-                        <p>Grey Eagle Casino offers a diverse and exciting gaming experience by partnering with some top
-                            software providers in the industry. By collaborating with Microgaming, Playtech, NetEnt,
-                            Evolution Gaming, and IGT, Grey Eagle Casino ensures that players have access to a wide
-                            range of high-quality games that cater to all preferences. Whether you're a fan of classic
-                            slots, live dealer games, or cutting-edge video slots, Grey Eagle Casino has something for
-                            everyone thanks to its partnership with these renowned software developers.
-                        </p>
-                    </div>
-                    <div>
-                        <h2 className="title">Security</h2>
-                        <p>At Grey Eagle Casino, security is paramount. The establishment employs cutting-edge technology
-                            and industry best practices to protect patron information and transactions. From encryption
-                            protocols to secure payment gateways, every aspect of the casino's operations is designed to
-                            safeguard against cyber threats and unauthorized access. Adhering to strict regulatory
-                            standards, Grey Eagle online Casino ensures a safe and fair gaming environment for all players.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-)
+          <div>
+            <h2 className={`title ${style.section__title}`}>
+              Software Technologies: Casino Rocket Bonus Codes
+            </h2>
+            <p>
+              The company that owns the Australian casino cooperates with many
+              well-known providers of gaming software. You have a great
+              opportunity to choose a game from your favorite provider, or try
+              games from new providers.
+            </p>
+            <br />
+            <p>
+              Among the most famous are BGaming, BetSoft, NetEnt, Habanero,
+              Evolution, Amatic and many others. Most of the pokies and some
+              other gambling games are available on the site in free demo mode.
+            </p>
+          </div>
+          <div>
+            <h2 className={`title ${style.section__title}`}>
+              Security: Rocket Play Casino No Deposit Sign Up Bonus
+            </h2>
+            <p>
+              The service uses a multi-level security system, provides reliable
+              data encryption and strictly adheres to the privacy policy.
+              Therefore, playing here is absolutely safe.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
