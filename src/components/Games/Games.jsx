cdfd54f8d30b860831fa1f857ecf47f668b542a2
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2024-08-30 15:02:58 +0300

    add games

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -1,55 +1,77 @@
-"use client"
-import style from "./Games.module.scss"
+"use client";
+import style from "./Games.module.scss";
 import Button from "@/components/Button/Button";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
-export default function Games ({firstId}) {
-    const [dataLoaded, setDataLoaded] = useState(false);
-    const [data, setData] = useState(null);
+export default function Games({ firstId }) {
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://api.adkey-seo.com/api/website/get-games/gambling',);
-                const loadData = await response.json();
-                setData(loadData);
-                setDataLoaded(true);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.adkey-seo.com/api/website/get-games/gambling"
+        );
+        const loadData = await response.json();
+        setData(loadData);
+        setDataLoaded(true);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-        if (!dataLoaded) {
-            fetchData();
-        }
-    }, [dataLoaded]);
-    return !!dataLoaded && (
-        <section className="section" id="games">
-            <div className="container">
-                <h2 className="title">TOP <span className="accent">GAMES</span></h2>
-               <ul className={style.list}>
-                   {data.map(game => {
-                       return (<li key={game.id}>
-                           <div className={style.cardBox}>
-                               <div className={style.card}>
-                                   <div className={style.cardFront}>
-                                       <img src={`https://api.adkey-seo.com/storage/images/games/${game.image}`} alt={game.name} loading="lazy" />
-                                   </div>
-                                   <div className={style.cardBack}>
-                                       <a href={`/casino/${firstId}`}>
-                                       <p>{game.name}</p>
-                                       <span><Image src="/images/play.svg" alt="play" loading="lazy" width={33} height={33}/> Play now</span>
-                                       </a>
-                                   </div>
-                               </div>
-                           </div>
-
-                       </li>)
-                   })}
-               </ul>
-                <Button href={`/casino/${firstId}`} size="big" center>All games</Button>
-            </div>
-        </section>
+    if (!dataLoaded) {
+      fetchData();
+    }
+  }, [dataLoaded]);
+  return (
+    !!dataLoaded && (
+      <section className={`section ${style.section__games}`} id="games">
+        <div className="container">
+          <h2 className="title">
+            TOP <span className="accent">GAMES</span>
+          </h2>
+          <ul className={style.list}>
+            {data.map((game) => {
+              return (
+                <li key={game.id}>
+                  <div className={style.cardBox}>
+                    <div className={style.card}>
+                      <div className={style.cardFront}>
+                        <img
+                          src={`https://api.adkey-seo.com/storage/images/games/${game.image}`}
+                          alt={game.name}
+                          loading="lazy"
+                        />
+                      </div>
+                      <div className={style.cardBack}>
+                        <a href={`/casino/${firstId}`}>
+                          <p>{game.name}</p>
+                          <span>
+                            <Image
+                              src="/images/play.svg"
+                              alt="play"
+                              loading="lazy"
+                              width={29}
+                              height={29}
+                            />{" "}
+                            Play now
+                          </span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+          <Button href={`/casino/${firstId}`} size="big" center>
+            All games
+          </Button>
+        </div>
+      </section>
     )
+  );
 }
