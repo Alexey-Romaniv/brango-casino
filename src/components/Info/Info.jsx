0731dfbdc317746915902a2fef1ac03749c447e8
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2024-08-30 19:18:52 +0300

    add info games

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,105 +1,125 @@
-import style from "./Info.module.scss"
+import style from "./Info.module.scss";
 
 export default function Info() {
-    return (
-        <section className={`section ${style.historySection}`}>
-            <div className="container">
-                <ul className={style.list}>
-                    <li>
-                        <h2 className="title">Grey Eagle Casino Games</h2>
-                        <p>
-                            One of the most intriguing aspects for potential visitors to this casino is the wide range
-                            of Grey Eagle casino games available, including slot machines, Baccarat, Blackjack,
-                            Roulette, Craps, Three Card Poker, and Ultimate Texas Hold’Em. Many players enjoy practicing
-                            online first, especially with less common games like Baccarat.
-                            <br/><br/>
-                            Exciting news for betting enthusiasts is the upcoming addition of new electronic tables at
-                            the casino, including Stadium Three Card Poker, Baccarat, and Stadium Blackjack on the
-                            Quartz Hybrid electronic table. With over 1,000 slot machines offering a variety of stakes
-                            from cents to $25 per spin, guests can find a game that suits their preferences.
-                            <br/><br/>
-                            High rollers can access the high-limit gaming area at Grey Eagle Casino and Resort,
-                            featuring over 40 slot machines and various table games in a 3,000 square feet space. Guests
-                            can also enjoy Baccarat, blackjack, and roulette, along with a private dining area for a
-                            break from the gaming action.
-                        </p>
-                    </li>
-                    <li>
-                        <h2 className="title">Grey Eagle Casino Poker</h2>
-                        <p>Poker enthusiasts at Grey Eagle Casino can indulge in their passion in a dedicated and
-                            exclusive poker room. The poker room offers a sophisticated and inviting environment for
-                            players to test their skills and compete against fellow enthusiasts. With games available
-                            from 2 pm to 2 am, players can enjoy a variety of poker variants and tournaments, adding an
-                            extra layer of excitement to their casino experience.
-                            <br/><br/>
-                            Whether you're a seasoned player or new to the game, the poker room at Grey Eagle Casino
-                            Calgary provides a thrilling and immersive setting to showcase your skills and enjoy the
-                            camaraderie of the poker community.
-                        </p>
-                    </li>
-                    <li>
-                        <h2 className="title">Grey Eagle Casino Bingo</h2>
-                        <p>
-                            Grey Eagle Casino offers an exciting Bingo experience for guests looking to add a twist to
-                            their gaming adventures. The Bingo hall at Grey Eagle Casino provides a vibrant and lively
-                            atmosphere where players can enjoy traditional Bingo games with a modern twist. With a
-                            variety of Bingo sessions scheduled throughout the week, guests can join in the fun and try
-                            their luck at winning exciting prizes. The spacious and comfortable Bingo hall ensures that
-                            players can relax and enjoy the game to the fullest. Whether you're a Bingo enthusiast or
-                            looking to try something new, the Bingo offerings at Grey Eagle Casino provide a fun and
-                            engaging experience for all players.
-                        </p>
-                    </li>
-                    <li>
-                        <h2 className="title">"Slots of Fun" and Other Promotions</h2>
-                        <p>
-                            Grey Eagle Casino goes above and beyond to provide its guests with an exceptional gaming
-                            experience through a variety of enticing bonuses and promotions. For those seeking a
-                            thrilling evening, the "Slots of Fun" event is a must-attend, featuring a free monthly slot
-                            tournament held on the last Monday of every month. Participants have the chance to compete
-                            for a grand prize of $1,000, making it an exciting opportunity for slot enthusiasts to test
-                            their luck and skills. Registration for this exhilarating event opens at 6:00 pm at guest
-                            services, so mark your calendars and get ready to spin your way to potential winnings.
-                        </p>
-                    </li>
-                    <li>
-                        <h2 className="title">Grey Eagle Casino Rewards Program </h2>
-                        <p>
-                            For those looking to maximize their rewards and benefits, the Winner's Edge rewards program
-                            at Grey Eagle Casino offers an array of exclusive promotions, discounts, and events for its
-                            members. By collecting points with each visit to the Calgary casino, rewards members can
-                            unlock opportunities for free food and other enticing rewards, enhancing their overall
-                            gaming experience or Grey Eagle Casino hotel accommodations. Joining the Winner's Edge
-                            program opens the door to a world of perks and privileges, ensuring that guests can make the
-                            most of their time at Grey Eagle Casino while enjoying top-notch rewards and benefits.z
-                        </p>
-                    </li>
-                    <li>
-                        <h2 className="title">Seniors Days</h2>
-                        <p>
-                            Seniors aged 55 and above are in for a treat with the "Seniors Days" promotion at Grey Eagle
-                            Casino, available every Sunday and Tuesday from 11 AM to 3 PM. During these special days,
-                            seniors can participate in hourly draws to win cash prizes equivalent to their age, adding
-                            an element of fun and excitement to their casino experience. Additionally, the last Sunday
-                            of each month brings "Super Sunday," where seniors have the chance to win a share of $1,500
-                            in cash.
-                        </p>
-                    </li>
-                    <li>
-                        <h2 className="title">Birthday Free Play</h2>
-                        <p>
-                            Celebrating a birthday at Grey Eagle Casino is made even more special with the "Birthday
-                            Free Play" promotion, offering guests $10 in Free Slot Play to enjoy on their special day.
-                            This exclusive offer can be redeemed on the birthday itself or the day before or after,
-                            allowing individuals to extend their birthday celebrations and potentially win big while
-                            playing their favorite slot games. It's a fantastic way to add some extra excitement and
-                            entertainment to your birthday festivities, creating lasting memories at Grey Eagle Casino.
-
-                        </p>
-                    </li>
-
-                </ul>
-            </div>
-        </section>
-    )
+  return (
+    <section className={`section ${style.historySection}`}>
+      <div className="container">
+        <ul className={style.list}>
+          <li>
+            <h2 className="title">
+              Games Available to Play: Casino Rocket Games
+            </h2>
+            <p>
+              Each Rocket Casino game causes incredible delight and allows you
+              to get bright, unforgettable emotions. The range of gambling
+              entertainment on this service is constantly expanding. At the same
+              time, thanks to the intuitive interface and convenient filters,
+              you can easily find your favorite game on the Rocket Play Casino
+              new website.
+            </p>
+          </li>
+          <div className={style.section__grid}>
+            <li>
+              <h2 className={`title ${style.section__title}`}>
+                Pokies: Casino Rocket FAQ
+              </h2>
+              <p>
+                You can Casino Rocket Play free, or use the real money mode.
+                Pokies are presented by more than 1000 unique pokies. Here you
+                can enjoy classic pokies, video slots, open a section with
+                popular games and much more.
+                <br />
+                You just need to create an account, get Rocket Play Casino no
+                deposit bonus or take advantage of the unique opportunity of the
+                demo mode. In this case, you don't even have to register. The
+                games are absolutely free. But without an account and money, you
+                will not be able to get Rocket Casino no deposit bonus. As well
+                as withdraw money in case of winning in demo mode.
+              </p>
+            </li>
+            <li>
+              <h2 className={`title ${style.section__title}`}>
+                Bitcoin Games: Rocket Play Casino Codes
+              </h2>
+              <p>
+                An important advantage and feature of Rocket Play Casino
+                Australia is the ability to play using cryptocurrency wallets.
+                The site has created a special section dedicated to games using
+                crypto currency.
+                <br />
+                Here you can realize the potential of your crypto wallet, place
+                a bet of almost any size and get a real chance to win. Due to
+                the uniqueness of cryptocurrency, your winnings can constantly
+                increase due to the changing exchange rate of cryptocurrencies.
+              </p>
+            </li>
+            <li>
+              <h2 className={`title ${style.section__title}`}>
+                Live Casino: Rocket Play Casino Bonus
+              </h2>
+              <p>
+                Many modern gamblers prefer to choose games where real dealers
+                participate. The uniqueness of such entertainment is that on
+                your screen you watch dealers who play the game in an exciting
+                way, create a unique atmosphere and bring us closer to the
+                conditions of land-based gambling houses.
+                <br />
+                At the same time, the choice of games with live dealers here is
+                incredible. This is an excellent reason to activate the Rocket
+                Play Casino promo code and get additional benefits, game money
+                and free spins.
+              </p>
+            </li>
+            <li>
+              <h2 className={`title ${style.section__title}`}>
+                Casino Rocket Poker{" "}
+              </h2>
+              <p>
+                To experience all the advantages of Rocket Play Casino real
+                money, be sure to try your hand at video poker. This is an
+                exciting and modern game. Here you will not have to rely only on
+                luck. Much depends on you.
+                <br />
+                Therefore, train, learn to play poker, show your skills and
+                demonstrate a high level of play. Thanks to the unique
+                atmosphere of this casino and fair winnings, you will definitely
+                be satisfied.
+              </p>
+            </li>
+            <li>
+              <h2 className={`title ${style.section__title}`}>
+                Table Games: Casino Rocket Slots
+              </h2>
+              <p>
+                The Casino Rocket Play online service offers an impressive
+                selection of table games. These are such unique entertainments
+                as baccarat, blackjack, roulette, dice and much more.
+                <br />
+                In table games, you will have to show skills and abilities.
+                Carefully choose the type of game and the tables you want to sit
+                at. Depending on the level of training and bets, you can win
+                impressive winnings.
+              </p>
+            </li>
+            <li>
+              <h2 className={`title ${style.section__title}`}>
+                Jackpots: Casino Rocket No Deposit Bonus Codes
+              </h2>
+              <p>
+                Many customers come here not only for bright emotions, but also
+                for big winnings. And the most impressive wins can be obtained
+                thanks to jackpot games. That's why a separate collection of
+                gambling entertainments is collected here, in which really solid
+                money is played out.
+                <br />
+                Of course, there won't be many chances. But the probability of
+                winning is always present. You just need a little luck and
+                patience. And most importantly - learn to enjoy gambling.
+              </p>
+            </li>
+          </div>
+        </ul>
+      </div>
+    </section>
+  );
 }
