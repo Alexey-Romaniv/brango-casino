c04ad85f92487e6e903769782591138451018e8b
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2024-09-03 23:35:31 +0300

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,18 @@
+import Link from "next/link";
 import style from "./Footer.module.scss";
 import Image from "next/image";
 export default function Footer() {
   return (
     <footer className={style.footer}>
       <div className={`container ${style.wrapper}`}>
-        <a href="/#main" className={style.footerLogo}>
+        <Link href="/#main" className={style.footerLogo}>
           <Image
             src="/images/logo.svg"
             alt="Rocket Casino logo"
             width={114}
             height={44}
           />
-        </a>
+        </Link>
         <div>
           <ul className={`${style.footerList} ${style.listPayment}`}>
             <li className={style.footerItem}>
