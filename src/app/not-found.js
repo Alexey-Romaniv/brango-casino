b043ac1eaa77657036754fe1d74029984f65621b
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2024-09-03 19:21:47 +0300

    fix

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,13 @@
-import Link from 'next/link'
-
+import Button from "@/components/Button/Button";
 export default function NotFound() {
-    return (
-        <div className='not-found__box'>
-            <h2>404</h2>
-            <p>Page Not Found</p>
-            <Link href="/">Return Home</Link>
-        </div>
-    )
+  return (
+    <div className="not-found__box">
+      <h2>404</h2>
+      <p>The page you were looking for does not exist.</p>
+      <p>You may have mistyped the address or the page may have moved.</p>
+      <Button href={"/"} size="big">
+        Home page
+      </Button>
+    </div>
+  );
 }
